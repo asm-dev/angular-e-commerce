7c71892c860a168f92801f73a4d7b84acd254907
asm-dev <anasalamancamontero@gmail.com>
2025-05-14 11:11:46 +0100

    Init components, API service and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CategoriesComponent },
+  { path: 'category/:name', component: ProductsComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+];
